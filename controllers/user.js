b19f226538a927869da25e2ea5fd7ea3a6d7d1a8
David Bzura <bzuradavid@gmail.com>
2020-09-17 08:46:25 -0300

    user sign in

--- controllers/user.js
+++ controllers/user.js
@@ -1,16 +1,41 @@
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+const expressJwt = require('express-jwt')
+const {errorHandler} = require('../helpers/dbErrorHandler')
 
 exports.signup = (req, res) => {
-    console.log('req.body', req.body);
+    // console.log('req.body', req.body);
     const user = new User(req.body);
     user.save((err, user) => {
         if (err) {
             return res.status(400).json({
-                err
+                err: errorHandler(err)
             });
         }
+        user.salt = undefined
+        user.hashed_password = undefined
         res.json({
             user
         });
     })
 };
+
+exports.signin = (req, res) => {
+    const {email, password} = req.body
+    User.findOne({ email }, (err, user) => {
+        if (err || !user) {
+            return res.status(400).json({
+                error: 'No user registered with that email'
+            })
+        }
+        if (!user.authenticate(password)) {
+            return res.status(401).json({ 
+                error: "e-mail or password don't match" 
+            });
+        }
+        const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET)
+        res.cookie('t', token, { expire: new Date() + 9999 })
+        const { _id, name, email, role } = user;
+        return res.json({ token, user: { _id, email, name, role } });
+    })
+}
